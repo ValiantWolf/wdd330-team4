ee236513b8a23eb28570bc105c17a63ecfb003c4
Yhoundeh <aarond96@byui.edu>
2021-02-09 11:28:11 -0700

    team project get api

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,7 @@ import ProductDetails from './productDetails.js';
 import { getParams } from './utils.js';
 
 const productId = getParams('product');
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData();
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
